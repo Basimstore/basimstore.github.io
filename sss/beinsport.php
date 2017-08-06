b97efdd680780d3e7169478bed7248a803f399b5
ba8im <alharbi.basim3@gmail.com>
2017-08-06 03:05:58 +0300

    Add SSS

---
+++ sss/beinsport.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="متخصص ب تطبيقات ابل وبرامج بلاس وبرامج مكرره">
+    <meta name="Author" content="Ba8im">
+    <title>Ba8im </title>
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/bootstrap.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/style.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/go.css">
+  </head>
+  <body>
+  <html dir=rtl lang=ar-sa><img alt="" height="100" src="img/begin.jpg" width="380" /><font color=red>
+  
+<br><br><br><br>
+
+<div class="container"    
+
+<div class="row">
+   
+
+
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/aa.png" alt="Snapchat++">
+      <div class="titleapp">adblock</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i> </div>
+     <div class="install"><a href="https://appsto.re/sa/P-e3cb.i"> <i class="fa fa-
+arrow-down" aria-hidden="true"></i> تحميل التطبيق </a></div>
+     <div class="des">  حمل هذا التطبيق لازالة الاعلانات من القنوات >التطبيق رسمي من الابستور </div>
+ 
+</div>
+
+
+    
+
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD1 </div> 
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+    <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein1.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة  </a></div>
+     <div class="des">  </div>
+    </div>
+    
+
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD2</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i> </div>
+    <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein2.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+    </div>
+
+   
+
+    
+
+    
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD3</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i></div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein3.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD4</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein4.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+    </div>
+
+    
+
+    
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD5</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i> </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein5.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">   </div>
+    </div>
+    
+   
+   
+   
+  
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD6</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein6.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+    </div>
+    
+ <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD7</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein7.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">   </div>
+    </div>
+   
+ <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beinsport HD8 </div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i>  </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein8.php"> <i class="fa fa-arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">   </div>
+  </div>
+  
+ <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beisport HD9</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i> </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein9.php"> <i class="fa fa-
+arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+ </div>
+  
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" id="appbox" style="height:464px;">
+      <img src="img/bein.png" alt="Snapchat++">
+      <div class="titleapp">beisport HD10</div>
+      <div class="ios"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+      <div class="ios"> <i class="fa fa-info-circle" aria-hidden="true"></i> </div>
+     <div class="install"><a href="http://m.livetv-free.com/channels/embed/bein10.php"> <i class="fa fa-
+arrow-down" aria-hidden="true"></i> مشاهدة </a></div>
+     <div class="des">  </div>
+ </div>
+
+
+
+
+<center>
+<p class="callme"> اتصل بنا تواصل معنا</p>
+  <div class="social">
+  <a href="https://www.instagram.com/ba8im/"> <i class="fa fa-instagram" aria-hidden="true"></i> </a>
+  <a href="https://www.snapchat.com/add/ba8im"> <i class="fa fa-snapchat" aria-hidden="true"></i> </a>
+  
+  <a href="#">  </a>
+</div>
+  <p>تصميم و برمجة : باسم الحميدي الحربي</p>
+</center>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+  </body>
+</html>                
---
+++ sss/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="متخصص ب تطبيقات ابل وبرامج بلاس وبرامج مكرره">
+    <meta name="Author" content="Ba8im">
+    <title>Ba8im</title>
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/bootstrap.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/style.css">
+    <link rel="stylesheet" href="http://www.i3zoozz.com/css/go.css">
+  </head>
+  <body>
+   <center>
+<html dir=rtl lang=ar-sa><img alt="" height="100" src="img/begin.jpg" width="350" /><font color=red> 
+</center>
+<center<br><br><br><br>
+<div class="container">
+  <center>
+     <div class="ii">
+     <div><button  onclick="window.location.href='/Sub1.php'">تطبيقات البلس</button></div>
+     <div><button  onclick="window.location.href='games.php'">برامج والعاب منوعة</button></div>
+     <div><button  href="#" onclick="window.location.href='/i3zoozz/beinsport.php'">قنوات Bein Sport مجانا  </button></div>
+     </div>
+  </center>
+</div>
+
+<center>
+<p class="callme"> اتصل بنا تواصل معنا</p>
+  <div class="social">
+  <a href="https://www.instagram.com/ba8im/"> <i class="fa fa-instagram" aria-hidden="true"></i> </a>
+  <a href="https://www.snapchat.com/add/ba8im"> <i class="fa fa-snapchat" aria-hidden="true"></i> </a>
+  
+  <a href="#">  </a>
+</div>
+  <p>تصميم و برمجة : باسم الحميدي الحربي</p>
+</center>
+
+
+<script src="http://www.i3zoozz.com/js/jquery-3.2.1.min.js"></script>
+<script src="http://www.i3zoozz.com/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
